06cb7312fb5a054ce0d3b12cc611e208355ae8e4
Max Korevsky <m.korevsky@admon.ai>
2024-03-12 09:01:38 +0300

    added indents

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 
 export const server = express();
+
 server.get('/hello', (req: Request, res: Response) => {
   const { 'user-name': userName } = req.query;
+
   res.send(`Hello ${userName}!`);
 });
